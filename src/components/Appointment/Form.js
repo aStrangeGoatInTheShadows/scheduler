2298fcf75899524399509f32868d3060fbfe2f56
Matt Macdonald <mksmacdonald@gmail.com>
2021-05-12 08:34:51 -0700

    notes for form build

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Form(props) {
+  return <></>;
+}
+
+/* MARK AS YOU GOs
+The Form component should track the following state:
+
+name:String
+interviewer:Number
+The Form component should have the following actions:
+
+setName:Function
+setInterviewer:Function
+The Form component should take the following props:
+
+name:String
+interviewers:Array
+interviewer:Number
+onSave:Function
+onCancel:Function
+*/
